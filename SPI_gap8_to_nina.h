b32423304de95f373e702c3c4b17b7e131c4272d
amos.newswanger <amos.newswanger@stud.tu-darmstadt.de>
2021-04-22 23:02:39 +0200

    Cleaned up root folder

--- SPI_gap8_to_nina.h
+++
@@ -1,7 +0,0 @@
-#ifndef SPI_GAP8_TO_NINA_H
-#define SPI_GAP8_TO_NINA_H
-
-static void send_char_SPI(void)
-int main(void)
-
-#endif
--- SPI_send_data.h
+++
@@ -1,7 +0,0 @@
-#ifndef SPI_SEND_DATA_H
-#define SPI_SEND_DATA_H
-
-static void send_data_SPI(PI_L2 void data)
-int main(void)
-
-#endif
